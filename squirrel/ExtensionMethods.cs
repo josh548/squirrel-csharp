bba14cb2efde24e4837d90bbc4bd19c59af0184a
Joshua Escamilla <joshua.escamilla@selu.edu>
2016-12-14 21:49:35 -0600

    Create List.Head() and List.Tail() extension methods

---
+++ squirrel/ExtensionMethods.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace squirrel
+{
+    public static class ExtensionMethods
+    {
+        public static T Head<T>(this List<T> list)
+        {
+            return list[0];
+        }
+
+        public static List<T> Tail<T>(this List<T> list)
+        {
+            return list.Skip(1).ToList();
+        }
+    }
+}
