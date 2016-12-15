4a47f8c4d97753e71d58db3db7642492bd59ea41
Joshua Escamilla <joshua.escamilla@selu.edu>
2016-12-14 23:21:36 -0600

    Create skeleton for Evaluator class with visit methods

---
+++ squirrel/Evaluator.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace squirrel
+{
+    public class Evaluator : Visitor
+    {
+        private readonly AstNode _root;
+
+        public Evaluator(AstNode root)
+        {
+            _root = root;
+        }
+
+        public AstNode Evaluate()
+        {
+            return Visit(_root, new Environment());
+        }
+
+        protected AstNode VisitInteger(AstNode node, Environment env)
+        {
+            return node;
+        }
+
+        protected AstNode VisitSymbol(AstNode node, Environment env)
+        {
+            return node;
+        }
+
+        protected AstNode VisitSymbolicExpression(AstNode node, Environment env)
+        {
+            if (node.Children.Count < 2)
+            {
+                throw new ArgumentException("symbolic expression must contain a symbol and at least one argument");
+            }
+
+            var visitedChildren = new List<AstNode>();
+            foreach (var child in node.Children)
+            {
+                var visitedChild = Visit(child, env);
+                if (visitedChild.Type == NodeType.Error)
+                {
+                    return visitedChild;
+                }
+                visitedChildren.Add(visitedChild);
+            }
+
+            var head = visitedChildren.Head();
+
+            if (head.Type != NodeType.Symbol)
+            {
+                throw new ArgumentException("first item in symbolic expression must be a symbol");
+            }
+
+            var tail = visitedChildren.Tail();
+
+            throw new NotImplementedException("evaluation of symbolic expressions is not implemented yet");
+        }
+
+        protected AstNode VisitQuotedExpression(AstNode node, Environment env)
+        {
+            return node;
+        }
+    }
+}
