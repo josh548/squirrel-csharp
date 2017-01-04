bd1f6c7ea79b3341366528c7914c7242862d144d
Joshua Escamilla <joshua.escamilla@selu.edu>
2017-01-03 23:19:23 -0600

    Replace underscore with value of last expression in REPL

--- squirrel/Program.cs
+++ squirrel/Program.cs
@@ -1,9 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using Squirrel.Exceptions;
 using Squirrel.Nodes;
-using Squirrel.Tokens;
 
 namespace Squirrel
 {
@@ -23,8 +21,24 @@ namespace Squirrel
 
         private static void RunFile(string path)
         {
-            var environment = new Environment();
-            Interpret(File.ReadAllText(path), ref environment);
+            var text = File.ReadAllText(path);
+            var env = new Environment();
+            INode result;
+            try
+            {
+                result = Interpret(text, ref env);
+            }
+            catch (TokenizerException e)
+            {
+                Console.WriteLine(e.Message);
+                return;
+            }
+            catch (ParserException e)
+            {
+                Console.WriteLine(e.Message);
+                return;
+            }
+            Console.WriteLine(result);
         }
 
         private static void RunInteractiveConsole()
@@ -32,8 +46,9 @@ namespace Squirrel
             var env = new Environment();
 
             const string prompt = ">>> ";
+            var lastOutput = "";
 
-            Console.WriteLine("Press ^D to quit");
+            Console.WriteLine("Press ^D to quit. Use an underscore ('_') to refer to the last output.");
             while (true)
             {
                 Console.Write(prompt);
@@ -43,45 +58,39 @@ namespace Squirrel
                 {
                     break;
                 }
+
+                line = line.Replace("_", lastOutput);
                 if (string.IsNullOrWhiteSpace(line))
                 {
                     continue;
                 }
-                Interpret(line, ref env);
+
+                INode result;
+                try
+                {
+                    result = Interpret(line, ref env);
+                }
+                catch (TokenizerException e)
+                {
+                    Console.WriteLine(e.Message);
+                    continue;
+                }
+                catch (ParserException e)
+                {
+                    Console.WriteLine(e.Message);
+                    continue;
+                }
+                Console.WriteLine(result);
+                lastOutput = result.ToString();
             }
         }
 
-        private static void Interpret(string text, ref Environment env)
+        private static INode Interpret(string text, ref Environment env)
         {
             var tokenizer = new Tokenizer(text);
-
-            List<Token> tokens;
-            try
-            {
-                tokens = tokenizer.Tokenize();
-            }
-            catch (TokenizerException e)
-            {
-                Console.WriteLine(e.Message);
-                return;
-            }
-
-            var parser = new Parser(tokens);
-
-            INode root;
-            try
-            {
-                root = parser.Parse();
-            }
-            catch (ParserException e)
-            {
-                Console.WriteLine(e.Message);
-                return;
-            }
-
-            var evaluator = new Evaluator(root);
-
-            Console.WriteLine(evaluator.Evaluate(ref env));
+            var parser = new Parser(tokenizer.Tokenize());
+            var evaluator = new Evaluator(parser.Parse());
+            return evaluator.Evaluate(ref env);
         }
     }
 }
