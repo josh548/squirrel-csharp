9f353507afdafb767241f50e6b4b5d3bc5055090
Joshua Escamilla <joshua.escamilla@selu.edu>
2017-01-13 21:42:30 -0600

    Create negative test for getting values from nested environments

--- test/test-library/EnvironmentTests.cs
+++ test/test-library/EnvironmentTests.cs
@@ -10,14 +10,14 @@ namespace Tests
         private static readonly INode TestVariableValue = new IntegerNode(1);
 
         [Fact]
-        public void TestGetValueFromCurrentEnvironment() {
+        public void TestCanGetValueFromCurrentEnvironment() {
             var environment = new Environment();
             environment.Put(TestVariableName, TestVariableValue);
             Assert.Equal(TestVariableValue, environment.Get(TestVariableName));
         }
 
         [Fact]
-        public void TestGetValueFromParentEnvironment() {
+        public void TestCanGetValueFromParentEnvironment() {
             var parentEnvironment = new Environment();
             var childEnvironment = new Environment(parentEnvironment);
             parentEnvironment.Put(TestVariableName, TestVariableValue);
@@ -25,12 +25,20 @@ namespace Tests
         }
 
         [Fact]
-        public void TestGetValueFromGrandparentEnvironment() {
+        public void TestCanGetValueFromGrandparentEnvironment() {
             var grandparentEnvironment = new Environment();
             var parentEnvironment = new Environment(grandparentEnvironment);
             var childEnvironment = new Environment(parentEnvironment);
             grandparentEnvironment.Put(TestVariableName, TestVariableValue);
             Assert.Equal(TestVariableValue, childEnvironment.Get(TestVariableName));
+        }
+
+        [Fact]
+        public void TestCannotGetValueFromChildEnvironment() {
+            var parentEnvironment = new Environment();
+            var childEnvironment = new Environment(parentEnvironment);
+            childEnvironment.Put(TestVariableName, TestVariableValue);
+            Assert.NotEqual(TestVariableValue, parentEnvironment.Get(TestVariableName));
         }
     }
 }
